37cf9d4e7d4b473abefe428d2984609527fad2aa
teo <teo.guichoux08@gmail.Com>
2023-01-19 12:12:29 +0100

    suite de clip, pas encore fini

--- CLIP/clip.py
+++ CLIP/clip.py
@@ -47,37 +47,110 @@ class AttentionBlock(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, embedding_size, seq_length, num_heads, n_blocks) -> None:
+    def __init__(self, embedding_size, seq_length, num_heads, n_blocks, vocab_size) -> None:
         super().__init__()
-        self.positional_encoding = nn.Parameters(torch.randn(seq_length, embedding_size))
+        self.vocab_size = vocab_size
+        self.n_blocks = n_blocks
+        self.num_heads = num_heads
+
+        self.positional_encoding = nn.Parameter(torch.empty(seq_length, embedding_size))
+        self.token_embedding = nn.Embedding(vocab_size, embedding_size)
+
         # create list of attention block
+        attention_blocks = []
+        for i in range(n_blocks):
+            attention_blocks.append(AttentionBlock(embedding_size=embedding_size, num_heads=num_heads))
+        self.attention_blocks = nn.Sequential(*attention_blocks)
+
+        #may be needed: layer norm, linear from encoded vals to probs
+        
     def forward(self, x):
         #loop over the attention blocks
-        x = x + self.positional_encoding
-        pass
+        x = self.token_embedding(x)
+        x = x + self.positional_encoding(x)
+
+        x = self.attenion_blocks(x)
+        #linear + softmax
+        return x
+
 
 class VisionTransformer(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, embedding_size, num_heads, n_blocks, kernel_size, stride, output_dim, input_resolution) -> None:
         super().__init__()
         #conv layer to embed patches
         #classical transformer afterward
         #may need 2 layer normalizations (pre and post transformer as in openai)
+        
+        self.n_blocks = n_blocks
+        self.num_heads = num_heads
 
-    def forward(self):
-        pass
+        scale = embedding_size ** -0.5
+        self.positional_encoding = nn.Parameter(torch.empty(scale * torch.randn((input_resolution // kernel_size) ** 2 + 1, embedding_size)))
+        self.class_embedding = nn.Parameter(scale * torch.randn(embedding_size))
+
+        self.convolution = nn.Conv2d(in_channels=3, out_channels=embedding_size, kernel_size = kernel_size, stride=stride, bias=False)
+
+         # create list of attention block
+        attention_blocks = []
+        for i in range(n_blocks):
+            attention_blocks.append(AttentionBlock(embedding_size=embedding_size, num_heads=num_heads))
+        self.attention_blocks = nn.Sequential(*attention_blocks)
+
+        
+        self.proj = nn.Parameter(scale * torch.randn(embedding_size, output_dim))
+
+    def forward(self, x):
+        x = self.convolution(x)
+        x = x + self.positional_encoding(x)
+
+        x = self.attention_blocks(x)
+
+        return x @ self.proj
 
 
 class Clip(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, embedding_size, vision_embedding, seq_length, num_heads, n_blocks, vocab_size, output_dim, kernel_size, stride, input_resolution) -> None:
         super().__init__()
         #initialize text and image encoders
         #similarity matrix
+        self.text_encoder = Transformer(embedding_size=embedding_size,
+                                        seq_length=seq_length,
+                                        num_heads=num_heads,
+                                        n_blocks=n_blocks,
+                                        vocab_size=vocab_size
+                                        )
 
-    def forward_text(self):
-        pass
+        vision_heads = vision_embedding // 64
 
-    def forward_image(self):
-        pass
+        self.image_encoder = VisionTransformer(embedding_size=vision_embedding,
+                                               num_heads=num_heads,
+                                               n_blocks=n_blocks,
+                                               kernel_size=kernel_size,
+                                               stride=stride,
+                                               output_dim=output_dim, 
+                                               input_resolution=input_resolution)
 
-    def forward(self):
-        pass
+        self.logit_scale = nn.Parameter(torch.ones([]) * torch.log(1 / 0.07))
+
+    def encode_text(self, x):
+        return self.text_encoder(x)
+        
+    def encode_image(self, x):
+        return self.image_encoder(x)
+
+    def forward(self, image, text):
+        encoded_image = self.encode_image(image)
+        encoded_text = self.encode_text(text)
+
+
+        #this part is completely from openAI
+        # normalized features
+        encoded = encoded / encoded_image.norm(dim=1, keepdim=True)
+        encoded_text = encoded_text / encoded_text.norm(dim=1, keepdim=True)
+
+        # cosine similarity as logits
+        logit_scale = self.logit_scale.exp()
+        logits_per_image = logit_scale * encoded_image @ encoded_text.t()
+        logits_per_text = logits_per_image.t()
+
+        return logits_per_image, logits_per_text
---
+++ CLIP/main.py
@@ -0,0 +1,34 @@
+import clip
+import torch
+from PIL import Image
+import requests
+from transformers import CLIPProcessor, CLIPModel
+from importlib import reload
+reload(clip)
+from clip import Clip
+
+
+if __name__ == "__main__":
+    
+
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
+
+    inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+
+    print(model)
+    my_clip = Clip(embedding_size=512, #certain
+                   vision_embedding=768, #certain
+                   seq_length=77, #presque sur
+                   num_heads=7, #pas sur
+                   vocab_size=49408, #presque sur
+                   n_blocks=12, #certain
+                   output_dim=512, #certain
+                   kernel_size=32, #certain
+                   stride=32, #certain
+                   input_resolution=224) #par calcul ça donne ça
+
+    print(my_clip)
