93380d0ae02959659ce27950a2efe983d8222a05
teo <teo.guichoux08@gmail.Com>
2023-01-25 18:17:58 +0100

    CLIP fonctionne en forward, mais c'est pas la version de openai

--- CLIP/clip.py
+++ CLIP/clip.py
@@ -16,17 +16,22 @@ class Attentionlayer(nn.Module):
         self.num_heads = num_heads
         self.embedding_size = embedding_size
 
-    def forward(self, x):
-        q = self.query(x)
+    def forward(self, x, mask):
+        q = self.query(x) #b grid**2 embedding_size*num_heads
         k = self.key(x)
         v = self.value(x)
-        z = F.softmax(q @ k.transpose(1,2), dim=-1) * v / (self.embedding_size ** .25) # as suggested in Transformers from scratch
+        i = q @ k.transpose(1,2)
+
+        if mask is not None:
+            z = torch.bmm(F.softmax(i+mask, dim=-1),v) / (self.embedding_size ** .25) # as suggested in Transformers from scratch
+        else: 
+            z = torch.bmm(F.softmax(i, dim=-1), v) / (self.embedding_size ** .25) # as suggested in Transformers from scratch
 
         return self.feed_forward(z)
 
 
 class AttentionBlock(nn.Module):
-    def __init__(self, embedding_size, num_heads) -> None:
+    def __init__(self, embedding_size, num_heads, mask=None) -> None:
         super().__init__()
         self.attention_layer = Attentionlayer(embedding_size, num_heads)
 
@@ -39,43 +44,61 @@ class AttentionBlock(nn.Module):
             nn.Linear(embedding_size * 4, embedding_size)
         )
 
+        self.mask = mask
+
+    def attention(self, x):
+        return self.attention_layer(x, self.mask)
+
     def forward(self, x):
-        x_att = self.attention_layer(x)
+        x_att = self.attention(x)
         x_norm1 = self.norm1(x_att + x)
         x_ff = self.mlp(x_norm1)
         return self.norm2(x_ff + x_norm1)
 
 
 class Transformer(nn.Module):
-    def __init__(self, embedding_size, seq_length, num_heads, n_blocks, vocab_size) -> None:
+    def __init__(self, embedding_size, seq_length, num_heads, n_blocks, vocab_size, output_dim, use_mask=True) -> None:
         super().__init__()
         self.vocab_size = vocab_size
         self.n_blocks = n_blocks
         self.num_heads = num_heads
+        self.seq_length = seq_length
 
         self.positional_encoding = nn.Parameter(torch.empty(seq_length, embedding_size))
-        self.token_embedding = nn.Embedding(vocab_size, embedding_size)
+        self.token_embedding = nn.Embedding(vocab_size, embedding_size, padding_idx=0)
+
+        self.mask = self.build_mask() if use_mask else None
 
         # create list of attention block
         attention_blocks = []
         for i in range(n_blocks):
-            attention_blocks.append(AttentionBlock(embedding_size=embedding_size, num_heads=num_heads))
+            attention_blocks.append(AttentionBlock(embedding_size=embedding_size, num_heads=num_heads, mask=self.mask))
         self.attention_blocks = nn.Sequential(*attention_blocks)
 
-        #may be needed: layer norm, linear from encoded vals to probs
+        self.linear = nn.Linear(seq_length, 1)
+        self.norm = nn.LayerNorm(embedding_size)
+        self.proj = nn.Parameter(torch.rand(embedding_size, output_dim))
+
+    def build_mask(self):
+        mask = torch.empty(self.seq_length, self.seq_length)
+        mask.fill_(-float("inf"))
+        mask.triu_(1) #set 1 on the diagonal
+
+        return mask
         
     def forward(self, x):
         #loop over the attention blocks
-        x = self.token_embedding(x)
-        x = x + self.positional_encoding(x)
-
-        x = self.attenion_blocks(x)
+        x = self.token_embedding(x).swapaxes(0,1)
+        x = x + self.positional_encoding
+        x = self.attention_blocks(x)
         #linear + softmax
-        return x
+        x = self.linear(x.swapaxes(1,2)).squeeze(-1)
+        x = self.norm(x)
+        return x @ self.proj
 
 
 class VisionTransformer(nn.Module):
-    def __init__(self, embedding_size, num_heads, n_blocks, kernel_size, stride, output_dim, input_resolution) -> None:
+    def __init__(self, embedding_size, num_heads, n_blocks, kernel_size, stride, output_dim, input_resolution, use_mask=False) -> None:
         super().__init__()
         #conv layer to embed patches
         #classical transformer afterward
@@ -83,27 +106,40 @@ class VisionTransformer(nn.Module):
         
         self.n_blocks = n_blocks
         self.num_heads = num_heads
-
         scale = embedding_size ** -0.5
+
         self.positional_encoding = nn.Parameter(scale * torch.randn((input_resolution // kernel_size) ** 2 + 1, embedding_size))
-        self.class_embedding = nn.Parameter(scale * torch.randn(embedding_size))
+        self.cls = nn.Parameter(scale * torch.randn(embedding_size))
 
         self.convolution = nn.Conv2d(in_channels=3, out_channels=embedding_size, kernel_size = kernel_size, stride=stride, bias=False)
+
+        self.mask = self.build_mask() if use_mask else None
 
          # create list of attention block
         attention_blocks = []
         for i in range(n_blocks):
-            attention_blocks.append(AttentionBlock(embedding_size=embedding_size, num_heads=num_heads))
+            attention_blocks.append(AttentionBlock(embedding_size=embedding_size, num_heads=num_heads, mask=self.mask))
         self.attention_blocks = nn.Sequential(*attention_blocks)
-
         
-        self.proj = nn.Parameter(scale * torch.randn(embedding_size, output_dim))
+        self.norm = nn.LayerNorm(embedding_size)
+        self.proj = nn.Parameter(scale * torch.randn(embedding_size, output_dim))    
+
+    def build_mask(self):
+        mask = torch.empty(self.seq_length, self.seq_length)
+        mask.fill_(-float("inf"))
+        mask.triu_(1) #set 1 on the diagonal
+
+        return mask
 
     def forward(self, x):
-        x = self.convolution(x)
-        x = x + self.positional_encoding(x)
-
+        x = self.convolution(x) #b w grid grid (grid=input_res/kernel_size)
+        b, w, grid, grid = x.shape
+        x = x.flatten(2).swapaxes(1,2) #b grid**2 w
+        cls = self.cls + torch.zeros(b, 1, w) # b 1 w
+        x = torch.cat((cls,x), dim=1) # b (input_res/kernel_size) **2 + 1 w
+        x = x + self.positional_encoding
         x = self.attention_blocks(x)
+        x = self.norm(x[:, 0, :])
 
         return x @ self.proj
 
@@ -117,8 +153,11 @@ class Clip(nn.Module):
                                         seq_length=seq_length,
                                         num_heads=num_heads,
                                         n_blocks=n_blocks,
-                                        vocab_size=vocab_size
+                                        vocab_size=vocab_size,
+                                        output_dim=output_dim,
+                                        use_mask=True
                                         )
+
 
         vision_heads = vision_embedding // 64
         self.image_encoder = VisionTransformer(embedding_size=vision_embedding,
@@ -131,6 +170,7 @@ class Clip(nn.Module):
 
         self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
 
+
     def encode_text(self, x):
         return self.text_encoder(x)
         
@@ -141,14 +181,14 @@ class Clip(nn.Module):
         encoded_image = self.encode_image(image)
         encoded_text = self.encode_text(text)
 
-
         #this part is completely from openAI
         # normalized features
-        encoded = encoded / encoded_image.norm(dim=1, keepdim=True)
+        encoded_image = encoded_image / encoded_image.norm(dim=1, keepdim=True)
         encoded_text = encoded_text / encoded_text.norm(dim=1, keepdim=True)
 
         # cosine similarity as logits
         logit_scale = self.logit_scale.exp()
+        
         logits_per_image = logit_scale * encoded_image @ encoded_text.t()
         logits_per_text = logits_per_image.t()
 
--- CLIP/main.py
+++ CLIP/main.py
@@ -1,6 +1,8 @@
 import clip
 import torch
 from PIL import Image
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
 import requests
 from transformers import CLIPProcessor, CLIPModel
 from importlib import reload
@@ -11,7 +13,7 @@ from clip import Clip
 if __name__ == "__main__":
     
 
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    #model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
     processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
     url = "http://images.cocodataset.org/val2017/000000039769.jpg"
@@ -19,11 +21,12 @@ if __name__ == "__main__":
 
     inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
 
-    print(model)
+    #print(model)
+    #print(sum(p.numel() for p in model.parameters()))
     my_clip = Clip(embedding_size=512, #certain
                    vision_embedding=768, #certain
                    seq_length=77, #presque sur
-                   num_heads=7, #pas sur
+                   num_heads=8, #sur
                    vocab_size=49408, #presque sur
                    n_blocks=12, #certain
                    output_dim=512, #certain
@@ -31,5 +34,15 @@ if __name__ == "__main__":
                    stride=32, #certain
                    input_resolution=224) #par calcul ça donne ça
 
-    print(my_clip)
+    #print(my_clip)
     #openai multihead attention != Transformer from scratch -> look at the doc of F.multihead_attention_forward
+    print(inputs.keys())
+    img = inputs["pixel_values"]
+    text = torch.zeros((77,2)) #ex SOS a photo of a cat EOS
+    caption = [999, 1, 2, 3, 4, 5, 998]
+    for i in range(7):
+        text[i] = caption[i]
+    
+    text = text.to(torch.long)
+    
+    print(my_clip(img, text))
