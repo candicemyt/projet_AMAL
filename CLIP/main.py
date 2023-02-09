8237198cd404982f1790018b468e2626ae3bddfb
teo <teo.guichoux08@gmail.Com>
2023-02-09 17:20:15 +0100

    visualisation and my clip main

--- CLIP/main.py
+++ CLIP/main.py
@@ -12,39 +12,25 @@ import clip
 
 
 if __name__ == "__main__":
-    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    _, preprocess = clip.load("ViT-B/32", device="cpu")
+    model = Clip(embedding_size=512, #certain
+                vision_embedding=768, #certain
+                seq_length=77, #presque sur
+                num_heads=8, #sur
+                vocab_size=49408, #presque sur
+                n_blocks=12, #certain
+                output_dim=512, #certain
+                kernel_size=32, #certain
+                stride=32, #certain
+                input_resolution=224).to(device)
 
-    model, preprocess = clip.load("ViT-B/32", device="cpu")
-    #model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    model.load_state_dict(torch.load("./my_clip_weights/clip.pth"))
 
     url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     image = Image.open(requests.get(url, stream=True).raw)
+    texts = ["an image of a cat", "an image of a duck", "an image of a dog"]
 
-    inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+    text_inputs = clip.tokenize(texts)
+    image_input = preprocess(image)
 
-    print(model)
-    #print(sum(p.numel() for p in model.parameters()))
-    my_clip = Clip(embedding_size=512, #certain
-                   vision_embedding=768, #certain
-                   seq_length=77, #presque sur
-                   num_heads=8, #sur
-                   vocab_size=49408, #presque sur
-                   n_blocks=12, #certain
-                   output_dim=512, #certain
-                   kernel_size=32, #certain
-                   stride=32, #certain
-                   input_resolution=224) #par calcul ça donne ça
-
-    #print(my_clip)
-    #openai multihead attention != Transformer from scratch -> look at the doc of F.multihead_attention_forward
-    print(inputs.keys())
-    img = inputs["pixel_values"]
-    text = torch.zeros((77,2)) #ex SOS a photo of a cat EOS
-    caption = [999, 1, 2, 3, 4, 5, 998]
-    for i in range(7):
-        text[i] = caption[i]
-    
-    text = text.to(torch.long)
-    
-    print(my_clip(img, text))
